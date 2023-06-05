e7d80aa990ad3cfa3d77330bf9624ff907ce3011
lht99 <33472127+lht99@users.noreply.github.com>
2023-06-05 15:34:21 +0900

    Update main.py
    Fb function changed (l, fb1)

--- main.py
+++ main.py
@@ -43,11 +43,10 @@ def handle_request():
       source_url = source_tag['src']
       return source_url
     
-    def fb(li):
+    def fb1(l):
       api = 'https://x2download.app/api/ajaxSearch/facebook'
-      headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.57'}
-      data = {'q': li, 'vt': 'facebook'}
-      response1 = r.post(api, data = data, headers = headers)
+      data = {'q': l, 'vt': 'facebook'}
+      response1 = r.post(api, data = data)
       url = response1.json()['links']
       for key in url:
         if key =='hd':
@@ -66,8 +65,8 @@ def handle_request():
       else:
         tuan = insta(data)
       return {"link": tuan, "author": "Le Tuan"}
-    elif ("facebook" in data) or ('fb.watch' in data):
-      tuan = fb(data)
+    elif "facebook" in data or "fb.watch" in data:
+      tuan = fb1(data)
       return {"link": tuan, "author": "Le Tuan"}
     else:
       return 'You sent an unsupported link. Please check link'
